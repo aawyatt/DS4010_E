eed684b625378c094d1eb2b4c19989b63d69c4ca
Jonah-Landas <landasjonah@gmail.com>
2025-02-17 12:02:52 -0600

    progress on webscraper and cleaning stuff

--- data_folder/raw/Reddit_Webscraped/DS401-Webscraper.py
+++ data_folder/raw/Reddit_Webscraped/DS401-Webscraper.py
@@ -5,12 +5,12 @@ import praw ##python reddit api wrapper
 from praw.models import MoreComments
 
 ##this driver is for chrome version 133 and up, make sure chrome is at least 133
-driver_path = "C:/Users/landa/OneDrive/Documents/DS401 Webscrapper/chromedriver-win64/chromedriver-win64/chromedriver.exe" ## put the path to your chrome driver here
+driver_path = "C:/Users/landa/OneDrive/Documents/DS 401 Project/DS401 Webscrapper/chromedriver-win64/chromedriver-win64/chromedriver.exe" ## put the path to your chrome driver here
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 
 ##praw user stuff
-user_agent = "ISU Dining Scraper v1.0 by /u/Necessary-Two1334"
+user_agent = "ISU Dining Scraper v1.0 by /u/Necessary-One1334"
 reddit = praw.Reddit (##setting up the whole reddit instance
 client_id="TIIqx-Q-WdqEj8YjpptfEg",  ##sent via email from reddit 
 client_secret="0H8yoW389pdU0YUVlqb8DNLf4zHyfw",  ##got from reddit developed applications page
@@ -18,7 +18,7 @@ user_agent=user_agent
 )
 
 
-keywords = ["Dining", "Conversations", "Convos", "Seasons", "Dining Hall", "Windows", "Friley Windows", "UDCC", "Dining Hall Food", "union drive community center"]
+keywords = ["UDM","Dining", "Conversations", "Convos", "Seasons", "Dining Hall", "Windows", "Friley Windows", "UDCC", "Dining Hall Food", "union drive community center"]
 Data = set ( )     ##empty set for the title of all of the reddit posts relating to dining halls at ISU
 #Descriptions = set( )       ## empyt set for all of the main descriptions of the post
 #Comments = set( )
@@ -30,13 +30,13 @@ for keyword in keywords:        ##iterates through all of the keywords given
         submission.comments.replace_more(limit=None) ##includes all sub-comments
         #Posts[Index] = submission.title + str(submission.num_comments) + str(submission.score) + str(submission.upvote_ratio) + submission.url
         #Index +=1
-        tup = ["Post",submission.selftext,submission.num_comments,submission.score,submission.upvote_ratio,submission.id,None,submission.url]
+        tup = ["Post",submission.selftext,submission.num_comments,submission.score,submission.upvote_ratio,submission.id,None,submission.created_utc,submission.url]
         Data.add(tuple(tup))                ##Adding title of main post to list
         print(tup)
         #Descriptions.add(submission.selftext)       ##Adding Description of the main post if it has text
         
         for comment in submission.comments.list():      ##using .list() lists every comment in the comment tree
-            Comment_tup = ["Comment",comment.body,len(comment.replies), comment.score,None,submission.id,comment.parent_id,comment.permalink] ##comments aren't given a ratio of upvotes and number of sub comments(from what i saw its to save space on reddits database)
+            Comment_tup = ["Comment",comment.body,len(comment.replies), comment.score,None,submission.id,comment.parent_id,comment.created_utc,comment.permalink] ##comments aren't given a ratio of upvotes and number of sub comments(from what i saw its to save space on reddits database)
             Data.add(tuple(Comment_tup))
             print(Comment_tup)
             #Comments.add(comment.body)                  ##adds all of the comments in each post
@@ -44,8 +44,8 @@ for keyword in keywords:        ##iterates through all of the keywords given
 
 # print(len(Descriptions))
 # print(len(Comments))
-Posts = pd.DataFrame(Data,columns=['Type','Body','Number of Comments','Number of Upvotes', 'Ratio of Upvotes','Submission ID','Parent ID','url'])
-Posts.to_csv("C:/Users/landa/OneDrive/Documents/DS401 Webscrapper/Post_Data.csv")
+Posts = pd.DataFrame(Data,columns=['Type','Body','Number of Comments','Number of Upvotes', 'Ratio of Upvotes','Submission ID','Parent ID','Date','url'])
+Posts.to_csv("C:/Users/landa/Documents/DS 401 Project/DS4010_E/data_folder/raw/Reddit_Webscraped/Post_Data.csv", index=False)
 
 print(Posts)
 
---
+++ data_folder/raw/Reddit_Webscraped/RedditCleaner.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import datetime
+
+## Plan: 
+## 1) since the scraper was run with multiple keywords on the same subreddit, there are likely duplicates so it would be good to first get rid of them 
+## 2) when webscraping, for some reason the apostrophy was mapped to â€™ so it would probably be best to remap that for sentiment analysis later
+## 3) iterate through all of the data looking for keywords like "dining hall", "convos", "seasons", etc
+## 4) Change the time from Unix time to match the dates from the other data (YYYY-MM-DD)
+## 5) Of all of that data, keep only those entries and drop the others. Will run a sentiment analysis algo later to get a value for each
+## 6) Run sentiment analysis algo
+## 7) after the sentiment value is found, it can be put into an equation which weights the sentiment, upvotes, proportion of upvotes to main post if its a comment, time posted(older gets a smaller weight), etc
+## 8) standardize this value to be between 1 and 5 and put this into the dataset, this can then be used for further analysis
+
+
+Reddit = pd.read_csv("C:/Users/landa/Documents/DS 401 Project/DS4010_E/data_folder/raw/Reddit_Webscraped/Post_Data.csv")
+Reddit = Reddit.filter(['Type','Body','Number of Comments','Number of Upvotes','Ratio of Upvotes','Submission ID', 'Parent ID','Date', 'url'])
+print(Reddit.columns)
+print(Reddit.shape)
+
+
+#Part 1
+Reddit = Reddit.drop_duplicates()
+print(len(Reddit))
+
+##Part 2
+cols = Reddit.columns.to_list()
+
+for i in range(len(Reddit)):
+    if "â€™" in str(Reddit['Body'][i]):
+        print(Reddit['Body'][i])
+        Reddit['Body'][i] = Reddit['Body'][i].replace("â€™","'")
+        print(Reddit['Body'][i])
+
+##Part 3
+
+data = set( )
+#"Food","food","Dining","dining","Dining Hall","dining hall",
+keywords = ["Friley","friley","frileys","Frileys","union drive community center","UDM","Conversations","conversations", "Convos","convos","UDCC","udcc","Windows","windows","Seasons","seasons"]
+badKeywords = ["Desktop", "Laptop","desktop", "Mail","mail","laptop", "Mac", "macbook", "career","Career"]
+
+for i in range(len(Reddit)):
+    if any(words in str(Reddit['Body'][i]) for words in keywords) and (Reddit['Type'][i] == "Post"):        ##Finding all posts with the given keywords
+        if(any(badWords in str(Reddit['Body'][i]) for badWords in badKeywords)):
+            continue
+        data.add(tuple(Reddit.iloc[i]))
+cleaned = pd.DataFrame(data,columns=['Type','Body','Number of Comments','Number of Upvotes', 'Ratio of Upvotes','Submission ID','Parent ID','Date','url'])
+print(cleaned.shape)
+cleaned = cleaned.drop_duplicates(subset=['Submission ID'])
+print(cleaned.shape)
+
+##Part 4
+#for i in range(len(cleaned)):
+#    cleaned['Date'][i] = datetime.datetime.fromtimestamp(cleaned['Date'][i]).strftime('%Y-%m-%d')
+#    if()
+
+##Part 5
+cleaned.to_csv("C:/Users/landa/Documents/DS 401 Project/DS4010_E/data_folder/raw/Reddit_Webscraped/Cleaned.csv", index=False)
+print(len(cleaned))
+
+##other parts on another script
+
