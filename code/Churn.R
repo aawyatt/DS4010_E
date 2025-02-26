64819138fb6eae318010100c8dc4978ca36fd7dd
Alison Wyatt <aawyatt@iastate.edu>
2025-02-26 11:07:15 -0600

    create and write 'MealPlanBySemester.csv' and 'RoomLocationBySemester.csv' to new 'clean' folder in the data folder

--- code/Churn.R
+++ code/Churn.R
@@ -7,9 +7,10 @@
 library(dplyr)
 library(stringr)
 library(ggplot2)
+library(tidyr)
 
 ## read in data
-data <- read.csv("data_folder/transformed_data/Combined_Data.csv")
+data <- read.csv("./data_folder/transformed_data/Combined_Data.csv")
 
 ## 'Spring Only XXXX' changed to 'Spring XXXX'
 data <- data %>%
@@ -18,6 +19,30 @@ data <- data %>%
                                            levels = c("Fall 2021", "Spring 2022", "Fall 2022", "Spring 2023", 
                                                       "Fall 2023", "Spring 2024", "Fall 2024", "Spring 2025")))
 
+## 1 row for each ID, meal plans (if applicable) for each semester
+reshaped_data <- data %>%
+  select(ID, Term.Session.Description, Meal.Plan.Description) %>%
+  arrange(ID, Term.Session.Description) %>%
+  pivot_wider(names_from = Term.Session.Description, values_from = Meal.Plan.Description,
+              values_fn = function(x) paste(unique(x), collapse = ", "), names_sort=TRUE)
+
+## View the transformed dataset
+head(reshaped_data)
+
+## Write tibble to dataframe then to csv
+df <- as.data.frame(reshaped_data)
+write.csv(df, "./data_folder/clean/MealPlanBySemester.csv", row.names = FALSE)
+
+## 1 row for each ID, room location (if applicable) for each semester
+byResidenceHall <- data %>%
+  select(ID, Term.Session.Description, Room.Location.Description) %>%
+  arrange(ID, Term.Session.Description) %>%
+  pivot_wider(names_from = Term.Session.Description, values_from = Room.Location.Description,
+              values_fn = function(x) paste(unique(x), collapse = ", "), names_sort=TRUE)
+
+## Write tibble to dataframe then to csv
+df2 <- as.data.frame(byResidenceHall)
+write.csv(df2, "./data_folder/clean/RoomLocationBySemester.csv", row.names = FALSE)
 
 ## group by ID
 grouped_data <- data %>% group_by(ID) %>%
