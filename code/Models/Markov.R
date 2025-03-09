955e36dc2f2cf0a40d2e02d9f4f6cbe02261ab24
caelliastate <caell@iastate.edu>
2025-03-09 16:43:08 -0500

    Counts of transitions

--- code/Models/Markov.R
+++ code/Models/Markov.R
@@ -6,7 +6,10 @@ library(dplyr)
 plans2 <- read.csv("./data_folder/clean/MealPlanBySemester.csv")
 plans2$Spring.2022 <- as.factor(plans2$Spring.2022)
 plans2$Fall.2022 <- as.factor(plans2$Fall.2022)
-ctoc2122 <- sum(plans2$Spring.2022 == 'Cardinal' & plans2$Fall.2022 == 'Cardinal')
+ctoc22 <- plans2 %>% count(Spring.2022, Fall.2022)
+ctoc23 <- plans2 %>% count(Spring.2022, Fall.2022)
+ctoc24 <- plans2 %>% count(Spring.2022, Fall.2022)
+
 
 #Transition matrix of probabilities between states
 states <- c("Cardinal", "Gold", "Campanile", "25MealBlocks", "50Mealblocks", "100MealBlocks", "NA")
