b501de2401cfa7cb99d8de195d61f60b4440a20a
caelliastate <caell@iastate.edu>
2025-03-13 12:03:19 -0500

    Add

--- code/Models/Markov.R
+++ code/Models/Markov.R
@@ -16,13 +16,13 @@ ctoc22_nona <- plans2 %>% filter(!(is.na(Spring.2022) & is.na(Fall.2022))) %>% c
 ctoc23_nona <- plans2 %>% filter(!(is.na(Spring.2023) & is.na(Fall.2023))) %>% count(Spring.2023, Fall.2023)
 ctoc24_nona <- plans2 %>% filter(!(is.na(Spring.2024) & is.na(Fall.2024))) %>% count(Spring.2024, Fall.2024) 
 
-ctoc22$Proportions <- ctoc22$n/nrow(plans2) * 100
-ctoc23$Proportions <- ctoc23$n/nrow(plans2) * 100
-ctoc24$Proportions <- ctoc24$n/nrow(plans2) * 100
+ctoc22$Proportions <- ctoc22$n/nrow(plans2)
+ctoc23$Proportions <- ctoc23$n/nrow(plans2)
+ctoc24$Proportions <- ctoc24$n/nrow(plans2)
 
-ctoc22_nona$Proportions <- ctoc22$n/sum(ctoc22$n) * 100
-ctoc23_nona$Proportions <- ctoc23$n/sum(ctoc23$n) * 100
-ctoc24_nona$Proportions <- ctoc24$n/sum(ctoc24$n) * 100
+ctoc22_nona$Proportions <- ctoc22_nona$n/sum(ctoc22_nona$n)
+ctoc23_nona$Proportions <- ctoc23_nona$n/sum(ctoc23_nona$n)
+ctoc24_nona$Proportions <- ctoc24_nona$n/sum(ctoc24_nona$n)
 
 #Transition matrix of probabilities between states
 states <- c("100 Meal Blocks", "25 Meal Blocks", "50 Meal Blocks", "Campanile", "Cardinal", "Gold", "NA")
