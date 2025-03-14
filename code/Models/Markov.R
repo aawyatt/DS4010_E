589ab47689ac1383efee1003ea12661c6f9d79d7
caelliastate <caell@iastate.edu>
2025-03-14 10:37:27 -0500

    Proportions and model prepped

--- code/Models/Markov.R
+++ code/Models/Markov.R
@@ -26,16 +26,27 @@ ctoc24_nona$Proportions <- ctoc24_nona$n/sum(ctoc24_nona$n)
 
 #Transition matrix of probabilities between states
 states <- c("100 Meal Blocks", "25 Meal Blocks", "50 Meal Blocks", "Campanile", "Cardinal", "Gold", "NA")
-transitionMatrix <- matrix(c())
+transitionMatrix <- matrix(c(0.0032206119, 0.0002800532, 0.0010501995, 0, 0, 0.0000700133, 0.0033606385, 
+                             0.0004200798, 0.0035706784, 0.0013302527, 0, 0, 0, 0.0101519289,
+                             0.0014702794, 0.0014702794, 0.0064412238, 0, 0, 0, 0.0118322481,
+                             0.0044808514, 0.0018203459, 0.0046908913, 0.0251347756, 0.0014702794, 0.0009801862, 0.0562206819,
+                             0.0098718757, 0.0051809844, 0.0108520619, 0.0336763985, 0.0627319191, 0.0109920885, 0.1801442274,
+                             0.0022404257, 0.0030105720, 0.0039907582, 0.0128124344, 0.0033606385, 0.0087516628, 0.0779248057,
+                             0.0100119023, 0.0113421550, 0.0125323812, 0.0200938178, 0.0473990058, 0.3310228943, 0), nrow = 7)
+
 
 
 #Creating the markov chain
 planChain <- new("markovchain", states = states, transitionMatrix = transitionMatrix)
 
+#Steady State
+states <- steadyStates(object = planChain)
+states
+
 #Simulate Markov Chain
 #Setting seed for reproducability
 set.seed(2025)
-modelStates <- rmarkovchain(n = 5, object = planChain, t0 = )
+modelStates <- rmarkovchain(n = 5, object = planChain, what = "list")
 
 
 
